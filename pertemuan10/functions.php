118eaa9662bf10091f7310f07c1c809ad3d00c34
robzdd <136417656+robzzy1922@users.noreply.github.com>
2024-06-06 20:09:14 +0700

    Add files via upload

---
+++ pertemuan10/functions.php
@@ -0,0 +1,37 @@
+<?php
+
+$conn = mysqli_connect("localhost","root","","flm");
+
+function query($query) {
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ( $row = mysqli_fetch_assoc($result) ) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function tambah($data){
+    global $conn;
+    
+    $judul = htmlspecialchars($data["judul"]);
+    $durasi = htmlspecialchars($data["durasi"]);
+    $genre = htmlspecialchars($data["genre"]);
+    $sutradara = htmlspecialchars($data["sutradara"]);
+    $rilis = htmlspecialchars($data["rilis"]);
+    $poster = htmlspecialchars($data["poster"]);
+
+    $query = "INSERT INTO list_film (judul, durasi, genre, sutradara, rilis, poster) VALUES('$judul','$durasi','$genre','$sutradara','$rilis','$poster')";
+    mysqli_query($conn,$query); 
+
+    return mysqli_affected_rows($conn);
+
+}
+
+function hapus($id){
+    global $conn;
+    mysqli_query($conn, "DELETE FROM list_film WHERE id = $id");
+
+    return mysqli_affected_rows($conn);
+}
